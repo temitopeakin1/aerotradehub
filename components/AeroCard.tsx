58cc2fa88c48156eb076e77d1e70ab1451c05ecd
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-01-16 08:15:27 +0100

    update

--- components/AeroCard.tsx
+++ components/AeroCard.tsx
@@ -5,14 +5,20 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 import { AeroProps } from "@/types";
 import AeroDetails from "./AeroDetails";
-import { calculateAeroRent} from "@/utils";
+import { calculateAeroRent } from "@/utils";
 
-interface AeroCardProps { 
+interface AeroCardProps {
   aero: AeroProps;
 }
 
 const AeroCard = ({ aero }: AeroCardProps) => {
-  const { manufacturer, model, range_nautical_miles, engine_type, cruise_speed_knots} = aero;
+  const {
+    manufacturer,
+    model,
+    range_nautical_miles,
+    engine_type,
+    cruise_speed_knots,
+  } = aero;
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -26,49 +32,71 @@ const AeroCard = ({ aero }: AeroCardProps) => {
         </h2>
       </div>
 
-      <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
-        <span className='self-start text-[14px] leading-[17px] font-semibold'>$</span>
+      <p className="flex mt-6 text-[32px] leading-[38px] font-extrabold">
+        <span className="self-start text-[14px] leading-[17px] font-semibold">
+          $
+        </span>
         {aeroRent}
-        <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
+        <span className="self-end text-[14px] leading-[17px] font-medium">
+          /day
+        </span>
       </p>
 
-      <div className='relative w-full h-40 my-3 object-contain'>
-            <Image src="/hero.svg" alt='aero model' fill priority className='object-contain' /> 
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.svg"
+          alt="aero model"
+          fill
+          priority
+          className="object-contain"
+        />
       </div>
 
-      <div className='relative flex w-full mt-2'>
-        <div className='flex group-hover:invisible w-full justify-between text-gray'>
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <Image src='/control-yoke.svg' width={20} height={20} alt='ctrl-yoke' />
-            <p className='text-[12px] leading-[17px]'>
-              {engine_type === "Jet" ? "Jet" : "Piston"}
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/control-yoke.svg"
+              width={20}
+              height={20}
+              alt="ctrl-yoke"
+            />
+            <p className="text-[12px] leading-[17px]">
+              {engine_type === "Jet"
+                ? "Jet"
+                : engine_type === "Piston"
+                ? "Piston"
+                : "Propjet"}
             </p>
           </div>
-          <div className='aero-card__icon'>
-            <Image src='/tire.svg' width={20} height={20} alt='tire.svg' />
-            <p className='aero-card__icon-text'>{cruise_speed_knots.toUpperCase()}
+          <div className="aero-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="tire.svg" />
+            <p className="aero-card__icon-text">
+              {cruise_speed_knots.toUpperCase()}
             </p>
           </div>
-          <div className='aero-card__icon'>
-            <Image src='/tire.svg' width={20} height={20} alt='tire.svg' />
-            <p className='aero-card__icon-text'>{model.toUpperCase()}
-            </p>
+          <div className="aero-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="tire.svg" />
+            <p className="aero-card__icon-text">{model.toUpperCase()}</p>
           </div>
-          </div>
+        </div>
 
         <div className="aero-card__btn-container">
-        <CustomButton
-            title='View More'
-            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
-            textStyles='text-white text-[14px] leading-[17px] font-bold'
-            rightIcon='/right-arrow.svg'
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
             handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
-      
 
-      <AeroDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} aero={aero} />
+      <AeroDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        aero={aero}
+      />
     </div>
   );
 };
